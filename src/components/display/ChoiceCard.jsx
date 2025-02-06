c595aa9628b8526a408f065ea0e0da6fe2e7cef4
Anubhav Jain <jainanubhav2003@gmail.com>
2025-02-06 12:02:23 +0530

    done

--- src/components/display/ChoiceCard.jsx
+++ src/components/display/ChoiceCard.jsx
@@ -4,7 +4,7 @@ import VisContext from "../../contexts/VisContext";
 import { GridContext } from "../../contexts/GridContext";
 
 const Card = props => {
-    const { setDisV, gridsVis, setSize } = useContext(VisContext);
+    const { setDisV, gridsVis, setSize, setUp } = useContext(VisContext);
     const { setGrid, setBlk } = useContext(GridContext);
 
     const n = props.n, m = n * n, dims = `${n}x${n}`, y = [1, -1, 0, 0], x = [0, 0, 1, -1];
@@ -13,7 +13,7 @@ const Card = props => {
         let r = n - 1, c = n - 1; const grid = [...Array(n)].map(
             (_, i) => [...Array(n)].map((_, j) => (i * n + j + 1) % m));
 
-        for (let k = 0; k < (n - 2) * 50; k++) {
+        for (let k = 0; k < 10; k++) {
             const val = Math.floor(Math.random() * 4), i = r + y[val], j = c + x[val];
 
             if (0 <= i && i < n && 0 <= j && j < n) {
@@ -21,8 +21,8 @@ const Card = props => {
             }
         }
 
-        setDisV("hidden"); setBlk([r, c]); setGrid(grid); setSize(n);
-        setTimeout(() => gridsVis[n - 3][1](""), 700);
+        setDisV("hidden"); setBlk([r, c]); setGrid(grid); setUp(1);
+        setSize(n); setTimeout(() => gridsVis[n - 3][1](""), 700);
     }
 
     return (
--- src/components/screen/WinScreen.jsx
+++ src/components/screen/WinScreen.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import VisContext from "../../contexts/VisContext";
 
 import "./css/WinScreen.css";
 
 const WinScreen = () => {
+    const { setDisV, gridsVis, size, setSize, setWinV, setUp } = useContext(VisContext);
+
+    const playAgain = () => { setWinV("hidden"); setTimeout(() => setDisV(""), 700); }
+
     return (
         <section className="px-5 py-4 bg-dark1 br-10">
             <p className="text-center text-light win-tag">You Win</p>
@@ -10,6 +16,11 @@ const WinScreen = () => {
             <div className="px-3 text-light text-box">
                 <p>Score: Coming Soon...</p>
                 <p>Time: Coming Soon...</p>
+            </div>
+
+            <div className="p-3">
+                <button className="w-100 p-2 btn my-btn br-10"
+                    onClick={playAgain}>Play Again</button>
             </div>
         </section>
     );
